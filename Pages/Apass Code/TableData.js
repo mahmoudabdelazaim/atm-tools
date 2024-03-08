ea6c96f06f29fa9853f294bf0eb2863694cafcb7
Mahmoud Abdel-azeem <39036935+mahmoudabdelazaim@users.noreply.github.com>
2024-03-08 18:49:29 +0200

    Add files via upload

---
+++ Pages/Apass Code/TableData.js
@@ -0,0 +1,257 @@
+////////// table containd data
+
+$(function(){
+//NBE table
+var NBE_U0= " nbeatm "					;$("#NBE_U0").html(NBE_U0);   // NBE username 0
+var NBE_P0= " @tm_87654321@TM "			;$("#NBE_P0").html(NBE_P0);   // NBE password 0
+
+//BM table
+var BM_U0= " atmbranch "				;$("#BM_U0").html(BM_U0);  // BM username 0
+var BM_P0= " Wincor@12345678 "			;$("#BM_P0").html(BM_P0);  // BM password 0
+
+//BDC table
+var BDC_U0= " administrator "			;$("#BDC_U0").html(BDC_U0);  // BDC username 0
+var BDC_P0= " Wincor@12345678 "			;$("#BDC_P0").html(BDC_P0);  // BDC password 0
+var BDC_U1= " administrator "			;$("#BDC_U1").html(BDC_U1);  // BDC username 1
+var BDC_P1= " Wincor "					;$("#BDC_P1").html(BDC_P1);  // BDC password 1
+
+//ENPO table
+var ENPO_U0= " administrator "			;$("#ENPO_U0").html(ENPO_U0);  // ENPO username 0
+var ENPO_P0= " enpo1234ENPO "			;$("#ENPO_P0").html(ENPO_P0);  // ENPO password 0
+var ENPO_U1= " administrator "			;$("#ENPO_U1").html(ENPO_U1);  // ENPO username 1
+var ENPO_P1= " Enpo "					;$("#ENPO_P1").html(ENPO_P1);  // ENPO password 1
+
+//ABE table
+var ABE_U0= " ABEDN "					;$("#ABE_U0").html(ABE_U0);  // ABE username 0
+var ABE_P0= " atm4007ADCAAMO "			;$("#ABE_P0").html(ABE_P0);  // ABE password 0
+
+//BOA table
+var BOA_U0= " administrator "			;$("#BOA_U0").html(BOA_U0);  // ABE username 0
+var BOA_P0= " Wincor "					;$("#BOA_P0").html(BOA_P0);  // ABE password 0
+
+var BOA_U1= " administrator "			;$("#BOA_U1").html(BOA_U1);  // ABE username 0
+var BOA_P1= " Wincor@12345678 "			;$("#BOA_P1").html(BOA_P1);  // ABE password 0
+
+var BOA_U2= " administrator "			;$("#BOA_U2").html(BOA_U2);  // ABE username 0
+var BOA_P2= " bank123BANK!@# "			;$("#BOA_P2").html(BOA_P2);  // ABE password 0
+
+var BOA_U3= " afm "						;$("#BOA_U3").html(BOA_U3);  // ABE username 0
+var BOA_P3= " Security.1 "				;$("#BOA_P3").html(BOA_P3);  // ABE password 0
+
+var BOA_U4= " afm "						;$("#BOA_U4").html(BOA_U4);  // ABE username 0
+var BOA_P4= " SECURITY.1 "				;$("#BOA_P4").html(BOA_P4);  // ABE password 0
+
+var BOA_U5= " afm "						;$("#BOA_U5").html(BOA_U5);  // ABE username 0
+var BOA_P5= " Wincor "					;$("#BOA_P5").html(BOA_P5);  // ABE password 0
+
+//CAE table
+var CAE_U0= " caeatmmanager	 "			;$("#CAE_U0").html(CAE_U0);  // ABE username 0
+var CAE_P0= " bank123BANK!@# "			;$("#CAE_P0").html(CAE_P0);  // ABE password 0
+
+var CAE_U1= " caeatmmanager	 "			;$("#CAE_U1").html(CAE_U1);  // ABE username 0
+var CAE_P1= " Wincor@12345678 "			;$("#CAE_P1").html(CAE_P1);  // ABE password 0
+
+var CAE_U2= " caeatmmanager	 "			;$("#CAE_U2").html(CAE_U2);  // ABE username 0
+var CAE_P2= " Wincor "					;$("#CAE_P2").html(CAE_P2);  // ABE password 0
+
+//FIB table
+//CAE table
+var FIB_U0= " administrator	 "			;$("#FIB_U0").html(FIB_U0);  // ABE username 0
+var FIB_P0= " DieboldNixdorf.1 "			;$("#FIB_P0").html(FIB_P0);  // ABE password 0
+
+var FIB_U1= " managesystem	 "			;$("#FIB_U1").html(FIB_U1);  // ABE username 0
+var FIB_P1= " diebold.nixdorf.1 "		;$("#FIB_P1").html(FIB_P1);  // ABE password 0
+
+//AWB table
+var AWB_U0= " administrator	 "			;$("#AWB_U0").html(AWB_U0);  // AWB username 0
+var AWB_P0= " AWB.Winc0r@22523 "		;$("#AWB_P0").html(AWB_P0);  // AWB password 0
+
+//SAIB table
+var SAIB_U0= " administrator "			;$("#SAIB_U0").html(SAIB_U0);  // AWB username 0
+var SAIB_P0= " Wincor@12345678 "		;$("#SAIB_P0").html(SAIB_P0);  // AWB password 0
+
+//TUB table
+var TUB_U0= " Administrator "			;$("#TUB_U0").html(TUB_U0);  //  username 0
+var TUB_P0= " 123WINCOR "				;$("#TUB_P0").html(TUB_P0);  //  password 0
+
+var TUB_U1= " Administrator "			;$("#TUB_U1").html(TUB_U1);  //  username 0
+var TUB_P1= " wincor.123WINCOR "		;$("#TUB_P1").html(TUB_P1);  //  password 0
+
+var TUB_U2= " Administrator "			;$("#TUB_U2").html(TUB_U2);  //  username 0
+var TUB_P2= " Wincor "					;$("#TUB_P2").html(TUB_P2);  //  password 0
+
+var TUB_U3= " atmbranch "				;$("#TUB_U3").html(TUB_U3);  //  username 0
+var TUB_P3= " 1234 "					;$("#TUB_P3").html(TUB_P3);  //  password 0
+
+//NBK table
+var NBK_U0= " .\manage_atm "			;$("#NBK_U0").html(NBK_U0);  //  username 0
+var NBK_P0= " Wincor@12345678 "			;$("#NBK_P0").html(NBK_P0);  //  password 0
+
+//AB table
+var AB_U0= " administrator "			;$("#AB_U0").html(AB_U0);  //  username 0
+var AB_P0= " Wincor "					;$("#AB_P0").html(AB_P0);  //  password 0
+
+//HDB table
+var HDB_U0= " Administrator "			;$("#HDB_U0").html(HDB_U0);  //  username 0
+var HDB_P0= " WINCOR.1234wincor "		;$("#HDB_P0").html(HDB_P0);  //  password 0
+
+//AIBK table
+var AIBK_U0= " administrator "			;$("#AIBK_U0").html(AIBK_U0);  //  username 0
+var AIBK_P0= " Wincor "					;$("#AIBK_P0").html(AIBK_P0);  //  password 0
+
+var AIBK_U1= " administrator "			;$("#AIBK_U1").html(AIBK_U1);  //  username 0
+var AIBK_P1= " DieboldNixdorf.01 "		;$("#AIBK_P1").html(AIBK_P1);  //  password 0
+
+//CIB table
+var CIB_U0= " Administrator "			;$("#CIB_U0").html(CIB_U0);  //  username 0
+var CIB_P0= " Wincor12345678 "			;$("#CIB_P0").html(CIB_P0);  //  password 0
+
+var CIB_U1= " Administrator "			;$("#CIB_U1").html(CIB_U1);  //  username 0
+var CIB_P1= " Wincor@12345678 "			;$("#CIB_P1").html(CIB_P1);  //  password 0
+
+var CIB_U2= " administrator "			;$("#CIB_U2").html(CIB_U2);  //  username 0
+var CIB_P2= " Wincor "					;$("#CIB_P2").html(CIB_P2);  //  password 0
+
+//ADIB table
+var ADIB_U0= " Administrator "			;$("#ADIB_U0").html(ADIB_U0);  //  username 0
+var ADIB_P0= " Wincor@12345678 "		;$("#ADIB_P0").html(ADIB_P0);  //  password 0
+
+//EGB table
+var EGB_U0= " administrator "			;$("#EGB_U0").html(EGB_U0);  //  username 0
+var EGB_P0= " BANK!@#bank123 "			;$("#EGB_P0").html(EGB_P0);  //  password 0
+
+//BRKA table
+var BRKA_U0= " DNadmin "				;$("#BRKA_U0").html(BRKA_U0);  //  username 0
+var BRKA_P0= " Wincor123 "				;$("#BRKA_P0").html(BRKA_P0);  //  password 0
+
+var BRKA_U1= " DNadmin "				;$("#BRKA_U1").html(BRKA_U1);  //  username 0
+var BRKA_P1= " Wincor124 "				;$("#BRKA_P1").html(BRKA_P1);  //  password 0
+
+var BRKA_U2= " DNadmin "				;$("#BRKA_U2").html(BRKA_U2);  //  username 0
+var BRKA_P2= " Wincor125 "				;$("#BRKA_P2").html(BRKA_P2);  //  password 0
+
+
+//QNB table
+var QNB_U0= " Wincor "					;$("#QNB_U0").html(QNB_U0);  //  username 0
+var QNB_P0= " P@$$w0rd@123456 "			;$("#QNB_P0").html(QNB_P0);  //  password 0
+
+
+//EALB table
+var EALB_U0= " administrator "			;$("#EALB_U0").html(EALB_U0);  //  username 0
+var EALB_P0= " Wincor "					;$("#EALB_P0").html(EALB_P0);  //  password 0
+
+
+//EF table
+var EF_U0= "   "						;$("#EF_U0").html(EF_U0);  //  username 0
+var EF_P0= "   "						;$("#EF_P0").html(EF_P0);  //  password 0
+
+
+//EURONET table
+var EURONET_U0= "   "					;$("#EURONET_U0").html(EURONET_U0);  //  username 0
+var EURONET_P0= "   "					;$("#EURONET_P0").html(EURONET_P0);  //  password 0
+
+//AUB table
+var AUB_U0= " administrator "			;$("#AUB_U0").html(AUB_U0);  //  username 0
+var AUB_P0= " Wincor@12345678 "			;$("#AUB_P0").html(AUB_P0);  //  password 0
+
+var AUB_U1= " username "				;$("#AUB_U1").html(AUB_U1);  //  username 0
+var AUB_P1= " password "				;$("#AUB_P1").html(AUB_P1);  //  password 0
+
+
+
+
+});
+
+////////////////// jqury table creator //////////////////////////////////////
+
+//NBE
+$('#NBE_A_Pass > tbody').append('<tr><td id="NBE_U0" width="120px"></td><td  id="NBE_P0"></td></tr>');
+
+//BM
+$('#BM_A_Pass > tbody').append('<tr><td id="BM_U0"  width="120px"></td><td  id="BM_P0"></td></tr>');
+
+//BDC
+$('#BDC_A_Pass > tbody').append('<tr><td id="BDC_U0"  width="120px"></td><td  id="BDC_P0"></td></tr>');
+$('#BDC_A_Pass > tbody').append('<tr><td id="BDC_U1"  width="120px"></td><td  id="BDC_P1"></td></tr>');
+
+//ENPO
+$('#ENPO_A_Pass > tbody').append('<tr><td id="ENPO_U0"  width="120px"></td><td  id="ENPO_P0"></td></tr>');
+$('#ENPO_A_Pass > tbody').append('<tr><td id="ENPO_U1"  width="120px"></td><td  id="ENPO_P1"></td></tr>');
+
+//ABE
+$('#ABE_A_Pass > tbody').append('<tr><td id="ABE_U0"  width="120px"></td><td  id="ABE_P0"></td></tr>');
+
+//BOA
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U0"  width="120px"></td><td  id="BOA_P0"></td></tr>');
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U1"  width="120px"></td><td  id="BOA_P1"></td></tr>');
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U2"  width="120px"></td><td  id="BOA_P2"></td></tr>');
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U3"  width="120px"></td><td  id="BOA_P3"></td></tr>');
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U4"  width="120px"></td><td  id="BOA_P4"></td></tr>');
+$('#BOA_A_Pass > tbody').append('<tr><td id="BOA_U5"  width="120px"></td><td  id="BOA_P5"></td></tr>');
+
+//CAE
+$('#CAE_A_Pass > tbody').append('<tr><td id="CAE_U0"  width="120px"></td><td  id="CAE_P0"></td></tr>');
+$('#CAE_A_Pass > tbody').append('<tr><td id="CAE_U1"  width="120px"></td><td  id="CAE_P1"></td></tr>');
+$('#CAE_A_Pass > tbody').append('<tr><td id="CAE_U2"  width="120px"></td><td  id="CAE_P2"></td></tr>');
+
+//FIB
+$('#FIB_A_Pass > tbody').append('<tr><td id="FIB_U0"  width="120px"></td><td  id="FIB_P0"></td></tr>');
+$('#FIB_A_Pass > tbody').append('<tr><td id="FIB_U1"  width="120px"></td><td  id="FIB_P1"></td></tr>');
+
+//AWB
+$('#AWB_A_Pass > tbody').append('<tr><td id="AWB_U0"  width="120px"></td><td  id="AWB_P0"></td></tr>');
+
+//SAIB
+$('#SAIB_A_Pass > tbody').append('<tr><td id="SAIB_U0"  width="120px"></td><td  id="SAIB_P0"></td></tr>');
+
+//TUB
+$('#TUB_A_Pass > tbody').append('<tr><td id="TUB_U0"  width="120px"></td><td  id="TUB_P0"></td></tr>');
+$('#TUB_A_Pass > tbody').append('<tr><td id="TUB_U1"  width="120px"></td><td  id="TUB_P1"></td></tr>');
+$('#TUB_A_Pass > tbody').append('<tr><td id="TUB_U2"  width="120px"></td><td  id="TUB_P2"></td></tr>');
+$('#TUB_A_Pass > tbody').append('<tr><td id="TUB_U3"  width="120px"></td><td  id="TUB_P3"></td></tr>');
+
+//NBK
+$('#NBK_A_Pass > tbody').append('<tr><td id="NBK_U0"  width="120px"></td><td  id="NBK_P0"></td></tr>');
+
+//NBK
+$('#AB_A_Pass > tbody').append('<tr><td id="AB_U0"  width="120px"></td><td  id="AB_P0"></td></tr>');
+
+//HDB
+$('#HDB_A_Pass > tbody').append('<tr><td id="HDB_U0"  width="120px"></td><td  id="HDB_P0"></td></tr>');
+
+//HDB
+$('#AIBK_A_Pass > tbody').append('<tr><td id="AIBK_U0"  width="120px"></td><td  id="AIBK_P0"></td></tr>');
+$('#AIBK_A_Pass > tbody').append('<tr><td id="AIBK_U1"  width="120px"></td><td  id="AIBK_P1"></td></tr>');
+
+//CIB
+$('#CIB_A_Pass > tbody').append('<tr><td id="CIB_U0"  width="120px"></td><td  id="CIB_P0"></td></tr>');
+$('#CIB_A_Pass > tbody').append('<tr><td id="CIB_U1"  width="120px"></td><td  id="CIB_P1"></td></tr>');
+$('#CIB_A_Pass > tbody').append('<tr><td id="CIB_U2"  width="120px"></td><td  id="CIB_P2"></td></tr>');
+
+//ADIB
+$('#ADIB_A_Pass > tbody').append('<tr><td id="ADIB_U0"  width="120px"></td><td  id="ADIB_P0"></td></tr>');
+
+//EGB
+$('#EGB_A_Pass > tbody').append('<tr><td id="EGB_U0"  width="120px"></td><td  id="EGB_P0"></td></tr>');
+
+//BRKA
+$('#BRKA_A_Pass > tbody').append('<tr><td id="BRKA_U0"  width="120px"></td><td  id="BRKA_P0"></td></tr>');
+$('#BRKA_A_Pass > tbody').append('<tr><td id="BRKA_U1"  width="120px"></td><td  id="BRKA_P1"></td></tr>');
+$('#BRKA_A_Pass > tbody').append('<tr><td id="BRKA_U2"  width="120px"></td><td  id="BRKA_P2"></td></tr>');
+
+//QNB
+$('#QNB_A_Pass > tbody').append('<tr><td id="QNB_U0"  width="120px"></td><td  id="QNB_P0"></td></tr>');
+
+//EALB
+$('#EALB_A_Pass > tbody').append('<tr><td id="EALB_U0"  width="120px"></td><td  id="EALB_P0"></td></tr>');
+
+//EF
+$('#EF_A_Pass > tbody').append('<tr><td id="EF_U0"  width="120px"></td><td  id="EF_P0"></td></tr>');
+
+//EURONET
+$('#EURONET_A_Pass > tbody').append('<tr><td id="EURONET_U0"  width="120px"></td><td  id="EURONET_P0"></td></tr>');
+
+//AUB
+$('#AUB_A_Pass > tbody').append('<tr><td id="AUB_U0"  width="120px"></td><td  id="AUB_P0"></td></tr>');
+$('#AUB_A_Pass > tbody').append('<tr><td id="AUB_U1"  width="120px"></td><td  id="AUB_P1"></td></tr>');
+
