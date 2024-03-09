808038983958bdb80a56aaa7eaced04f605f4910
Mahmoud Abdel-azeem <39036935+mahmoudabdelazaim@users.noreply.github.com>
2024-03-09 23:01:15 +0200

    Add files via upload

---
+++ Database/BIOS_DataTable.js
@@ -0,0 +1,39 @@
+////////////////// jqury table creator //////////////////////////////////////
+
+$('#BiosPass > tbody').append('<tr><th>RM4 @ V6 ATMs</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>DNDefault123</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>BDC</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>As12345</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>NBE</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>456789</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>Wincor12345</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>CAE</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>Admin</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>BOA</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>Security.1</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>Others</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>wincor12345</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>wincor123</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>Wincor@12345</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>wincor@12345</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>Wincor@123</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>wincor@123</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>ATM12345</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>87654321</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>12345678</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>987654</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>123456</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>111111</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>DNDefault</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>P@ssword</td></tr>');
+
+$('#BiosPass > tbody').append('<tr><th>AMT Configration Password</th></tr>');
+$('#BiosPass > tbody').append('<tr><td>Admin@123</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>admin</td></tr>');
+$('#BiosPass > tbody').append('<tr><td>admin@123</td></tr>');
+
