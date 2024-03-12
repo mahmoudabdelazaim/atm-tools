cb4ddd299a52c17f753e8ee92f68aa0a4f299559
Mahmoud Abdel-azeem <39036935+mahmoudabdelazaim@users.noreply.github.com>
2024-03-12 22:42:25 +0200

    Update HomePage.js

--- Database/HomePage.js
+++ Database/HomePage.js
@@ -9,6 +9,7 @@ $('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreads
 $('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
 $('#HomePage1 > tbody').append('<tr><td><a href="https://fa-ewrh-saasfaprod1.fa.ocs.oraclecloud.com/" class="lnk" target=”_blank”>Oracle Fusion HCM</a></td></tr>');
 $('#HomePage1 > tbody').append('<tr><td><a href="https://hrselfservice.rayacorp.com/HRSelfService/logon.aspx" class="lnk" target=”_blank”>HR Self Service</a></td></tr>');
+$('#HomePage1 > tbody').append('<tr><td><a href="Database/ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables.HTML" class="lnk" target=”_blank”>ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables</a></td></tr>');
 //homePage2
 $('#HomePage2 > tbody').append('<tr><td><a href="https://raya-atmtickets.com/login" class="lnk" target=”_blank”>Raya ATM Tickets</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="https://drive.google.com/drive/u/0/folders/1maW9l23lWX48MAjbT_fTx6obDKMtnSd2" class="lnk" target=”_blank”>Updated Accounts check Lists</a></td></tr>');
@@ -16,6 +17,7 @@ $('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreads
 $('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="https://fa-ewrh-saasfaprod1.fa.ocs.oraclecloud.com/" class="lnk" target=”_blank”>Oracle Fusion HCM</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="https://hrselfservice.rayacorp.com/HRSelfService/logon.aspx" class="lnk" target=”_blank”>HR Self Service</a></td></tr>');
+$('#HomePage2 > tbody').append('<tr><td><a href="Database/ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables.HTML" class="lnk" target=”_blank”>ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables</a></td></tr>');
 
 
 
