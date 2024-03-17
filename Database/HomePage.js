d59bacb8941dd3eda366ec018b4efcabcbd70843
Mahmoud Abdel-azeem <39036935+mahmoudabdelazaim@users.noreply.github.com>
2024-03-17 09:01:25 +0200

    Update HomePage.js

--- Database/HomePage.js
+++ Database/HomePage.js
@@ -4,20 +4,20 @@
 
 // HomePage1
 $('#HomePage1 > tbody').append('<tr><td><a href="https://raya-atmtickets.com/login" class="lnk" target=”_blank”>Raya ATM Tickets</a></td></tr>');
-$('#HomePage1 > tbody').append('<tr><td><a href="https://drive.google.com/drive/u/0/folders/1maW9l23lWX48MAjbT_fTx6obDKMtnSd2" class="lnk" target=”_blank”>Updated Accounts check Lists</a></td></tr>');
-$('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/1Ic6xh5vVGQDpIBgUJI4iN8BO1oCqepCLoa5nC6V8AF4/edit#gid=725233425" class="lnk" target=”_blank”>Golden Image Tracking Sheet</a></td></tr>');
-$('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
 $('#HomePage1 > tbody').append('<tr><td><a href="https://fa-ewrh-saasfaprod1.fa.ocs.oraclecloud.com/" class="lnk" target=”_blank”>Oracle Fusion HCM</a></td></tr>');
 $('#HomePage1 > tbody').append('<tr><td><a href="https://hrselfservice.rayacorp.com/HRSelfService/logon.aspx" class="lnk" target=”_blank”>HR Self Service</a></td></tr>');
 $('#HomePage1 > tbody').append('<tr><td><a href="Database/ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables.HTML" class="lnk" target=”_blank”>ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables</a></td></tr>');
+$('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/1Ic6xh5vVGQDpIBgUJI4iN8BO1oCqepCLoa5nC6V8AF4/edit#gid=725233425" class="lnk" target=”_blank”>Golden Image Tracking Sheet</a></td></tr>');
+$('#HomePage1 > tbody').append('<tr><td><a href="https://drive.google.com/drive/u/0/folders/1maW9l23lWX48MAjbT_fTx6obDKMtnSd2" class="lnk" target=”_blank”>Updated Accounts check Lists</a></td></tr>');
+$('#HomePage1 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
 //homePage2
 $('#HomePage2 > tbody').append('<tr><td><a href="https://raya-atmtickets.com/login" class="lnk" target=”_blank”>Raya ATM Tickets</a></td></tr>');
-$('#HomePage2 > tbody').append('<tr><td><a href="https://drive.google.com/drive/u/0/folders/1maW9l23lWX48MAjbT_fTx6obDKMtnSd2" class="lnk" target=”_blank”>Updated Accounts check Lists</a></td></tr>');
-$('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/1Ic6xh5vVGQDpIBgUJI4iN8BO1oCqepCLoa5nC6V8AF4/edit#gid=725233425" class="lnk" target=”_blank”>Golden Image Tracking Sheet</a></td></tr>');
-$('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="https://fa-ewrh-saasfaprod1.fa.ocs.oraclecloud.com/" class="lnk" target=”_blank”>Oracle Fusion HCM</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="https://hrselfservice.rayacorp.com/HRSelfService/logon.aspx" class="lnk" target=”_blank”>HR Self Service</a></td></tr>');
 $('#HomePage2 > tbody').append('<tr><td><a href="Database/ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables.HTML" class="lnk" target=”_blank”>ProCashNDC M-Status and M-Data Error Mapping Cross Reference Tables</a></td></tr>');
+$('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/1Ic6xh5vVGQDpIBgUJI4iN8BO1oCqepCLoa5nC6V8AF4/edit#gid=725233425" class="lnk" target=”_blank”>Golden Image Tracking Sheet</a></td></tr>');
+$('#HomePage2 > tbody').append('<tr><td><a href="https://docs.google.com/spreadsheets/d/19P41_qdESPnAjFbFwBFGdmpPzulB_GQdgcRHG1I9vMs/edit#gid=0" class="lnk" target=”_blank”>DN Cassette Configration</a></td></tr>');
+$('#HomePage2 > tbody').append('<tr><td><a href="https://drive.google.com/drive/u/0/folders/1maW9l23lWX48MAjbT_fTx6obDKMtnSd2" class="lnk" target=”_blank”>Updated Accounts check Lists</a></td></tr>');
 
 
 
